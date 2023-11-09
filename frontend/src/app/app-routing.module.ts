6d7f1712397bce0213ade335939c0929cf9942c6
lorenzo <adducilorenzo@hotmail.it>
2023-11-09 14:44:34 +0100

    added teacher dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TeacherDashboardComponent} from "./teacher-dashboard/teacher-dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'dashboard', component: TeacherDashboardComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherDashboardComponent
   ],
   imports: [
     BrowserModule,
