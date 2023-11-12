2731b55538b24004ba0518e140803d9851946db1
pit0500 <pietro.macri2000@gmail.com>
2023-11-12 18:52:43 +0100

    Created Questions models and serializers

---
+++ backend/questions/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    teacher = models.ForeignKey("Teacher", on_delete=models.CASCADE)
+    text = models.TextField()
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    answer_id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    correct = models.BooleanField()
---
+++ backend/questions/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+
+
+class QuestionViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def delete(self, request, pk=None):
+        pass
