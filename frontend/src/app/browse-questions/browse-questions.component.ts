1b7f2f634d310d893f2fdf2286ae435a7cf74eeb
Kirubel01 <ydbkbl92l18z315l@studenti.unical.it>
2023-12-04 23:33:30 +0100

    package-lock.jason updated

--- frontend/src/app/browse-questions/browse-questions.component.ts
+++ frontend/src/app/browse-questions/browse-questions.component.ts
@@ -1,13 +1,9 @@
-// src/app/browse-questions/browse-questions.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../services/item.service';
 import { QuestionService } from '../services/question.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import * as XLSX from 'xlsx';
 import * as FileSaver from 'file-saver';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatSelectModule } from '@angular/material/select';
 
 interface Answer {
   answer_id: number;
@@ -44,6 +40,7 @@ export class BrowseQuestionsComponent implements OnInit {
   showChoices: { [topic: string]: boolean[] } = {};
   showQuestions: boolean = false;
   selectedFilter: string = 'all'; // Default filter value
+  showExportButtons = false;
 
   constructor(
     private itemService: ItemService,
@@ -70,14 +67,12 @@ export class BrowseQuestionsComponent implements OnInit {
       'MultipleChoiceAnswers',
     ];
     const xlsxData: any[][] = [];
-
+  
     this.questionsGroupedByTopic.forEach((group) => {
       group.questions.forEach((question) => {
         if (question.latest_version) {
-          const answerTexts = question.answers
-            .map((answer) => answer.text)
-            .join(';');
-          const correctIndices = question.answers.reduce(
+          const answerTexts = question.answers.map((answer) => answer.text).join(';');
+          const correctIndices: number[] = question.answers.reduce(
             (indices: number[], answer, index) => {
               if (answer.correct) {
                 indices.push(index + 1); // Add 1 for 1-based indexing
@@ -86,63 +81,59 @@ export class BrowseQuestionsComponent implements OnInit {
             },
             []
           );
-
+  
           const correctIndicesString = correctIndices.join(';');
           xlsxData.push([
             question.question_id,
             question.topic,
             question.text,
             question.score,
-
             answerTexts,
             correctIndicesString,
           ]);
         }
       });
     });
-
+  
     const worksheet = XLSX.utils.aoa_to_sheet([header, ...xlsxData]);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'questions');
-    const xlsxOutput = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array',
-    });
+    const xlsxOutput = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     const blob = new Blob([xlsxOutput], { type: 'application/octet-stream' });
     FileSaver.saveAs(blob, 'questions.xlsx');
   }
+
   exportToJSON() {
-    {
-      const jsonData: any[] = [];
+    const jsonData: any[] = [];
 
-      this.questionsGroupedByTopic.forEach((group) => {
-        group.questions.forEach((question) => {
-          if (question.latest_version) {
-            const answerTexts = question.answers.map((answer) => ({
-              answer_id: answer.answer_id,
-              text: answer.text,
-              correct: answer.correct,
-            }));
+    this.questionsGroupedByTopic.forEach((group) => {
+      group.questions.forEach((question) => {
+        if (question.latest_version) {
+          const answerTexts = question.answers.map((answer) => ({
+            answer_id: answer.answer_id,
+            text: answer.text,
+            correct: answer.correct,
+          }));
 
-            const jsonQuestion = {
-              question_id: question.question_id,
-              teacher: question.teacher,
-              text: question.text,
-              score: question.score,
-              topic: question.topic,
-              answers: answerTexts,
-            };
+          const jsonQuestion = {
+            question_id: question.question_id,
+            teacher: question.teacher,
+            text: question.text,
+            score: question.score,
+            topic: question.topic,
+            answers: answerTexts,
+          };
 
-            jsonData.push(jsonQuestion);
-          }
-        });
+          jsonData.push(jsonQuestion);
+        }
       });
+    });
 
-      const jsonOutput = JSON.stringify(jsonData, null, 2);
-      const blob = new Blob([jsonOutput], { type: 'application/json' });
-      FileSaver.saveAs(blob, 'questions.json');
-    }
+    const jsonOutput = JSON.stringify(jsonData, null, 2);
+    const blob = new Blob([jsonOutput], { type: 'application/json' });
+    FileSaver.saveAs(blob, 'questions.json');
   }
+
   exportToCSV() {
     let csvData =
       '"ID";"Topic";"Text";"Score";"MultipleChoiceOptions";"MultipleChoiceAnswers"\n';
@@ -264,7 +255,7 @@ export class BrowseQuestionsComponent implements OnInit {
       );
     });
   }
-  showExportButtons = false;
+
   applyFilter() {
     this.showExportButtons = this.selectedFilter === 'latest';
     if (this.selectedFilter === 'latest') {
